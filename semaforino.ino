efe78bfcceff042bf4c21dcf0a50c0617b3fb262
Alessandro Brazzarola <alessandrobrz@users.noreply.github.com>
2021-07-04 21:27:48 +0200

    Update semaforino.ino

--- semaforino.ino
+++ semaforino.ino
@@ -2,23 +2,26 @@
 Traffic Light LED
 by alessandrobrz
 */
+int LED_Verde = 8;
+int LED_Giallo = 9;
+int LED_Rosso = 10;
 
 void setup() {
   // inizializzazione pin uscite
-  pinMode(8, OUTPUT);  //LED VERDE
-  pinMode(9, OUTPUT);  //LED GIALLO
-  pinMode(10, OUTPUT); //LED ROSSO
+  pinMode(LED_verde, OUTPUT);  //LED VERDE
+  pinMode(LED_Giallo, OUTPUT);  //LED GIALLO
+  pinMode(LED_Rosso, OUTPUT); //LED ROSSO
 }
 
 // CICLO DI LAVORO SEMAFORO A LED
 void loop() {
-  digitalWrite(8, HIGH);    // ACCENSIONE LED VERDE
+  digitalWrite(LED_verde, HIGH);    // ACCENSIONE LED VERDE
   delay(5000);              // ATTESA 5 SECONDI
-  digitalWrite(9, HIGH);    // ACCENSIONE LED GIALLO
+  digitalWrite(LED_Giallo, HIGH);    // ACCENSIONE LED GIALLO
   delay(5000);              // ATTESA DI 5 SECONDI
-  digitalWrite(8, LOW);     // SPEGNIMENTO LED VERDE
-  digitalWrite(9, LOW);     // SPEGNIMENTO LED GIALLO
-  digitalWrite(10, HIGH);   // ACCENSIONE LED ROSSO
+  digitalWrite(LED_verde, LOW);     // SPEGNIMENTO LED VERDE
+  digitalWrite(LED_Giallo, LOW);     // SPEGNIMENTO LED GIALLO
+  digitalWrite(LED_Rosso, HIGH);   // ACCENSIONE LED ROSSO
   delay(5000);              // ATTESA DI 5 SECONDI
-  digitalWrite(10, LOW);    // SPEGNIMENTO LED ROSSO
+  digitalWrite(LED_Rosso, LOW);    // SPEGNIMENTO LED ROSSO
 }
